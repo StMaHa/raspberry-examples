56e7d18e4033e68a3117f3680d051f33e624959e
StMa-Ha <github@inca-vikingo.de>
2019-07-14 17:55:47 +0200

    fix callback

--- callback-function/led-callback.py
+++ callback-function/led-callback.py
@@ -12,18 +12,23 @@ status = True
 led_blink = False
 
 def stop_function():   # Callback Funktion zum Programm beenden
+     global status
      status = False
 
 def led_function():     # Callback Funktion für die LED (blinken/leuchten)
+     global led_blink
      led_blink = True
 
 my_factory = PiGPIOFactory()
 
-led = LED(pin_status, pin_factory = my_factory)		# Initialisiere LED am GPIO-Pin 17
-schalter = Button(pin_button, hold_time = 2, pin_factory = my_factory)	# Initialisiere Button am GPIO-Pin 25
+# Initialisiere LED am GPIO-Pin 17
+led = LED(pin_status, pin_factory = my_factory)
+# Initialisiere Button am GPIO-Pin 25
+schalter = Button(pin_button, pull_up = True, hold_time = 2, pin_factory = my_factory)
 schalter.when_pressed = led_function
-schalter.when_held() = stop_function
+schalter.when_held = stop_function
 
+# Hauptschleife
 while status:
     if led_blink:
         led.toggle()
