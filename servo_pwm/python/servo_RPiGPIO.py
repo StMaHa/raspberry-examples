89e468f8ecbb2b9f0c80abf4ce2f3d529ed73ba3
Stefan Mahnke-Hartmann <12825445+StMaHa@users.noreply.github.com>
2023-12-01 13:11:48 +0100

    Adjust servo limits

--- servo_pwm/python/servo_RPiGPIO.py
+++ servo_pwm/python/servo_RPiGPIO.py
@@ -8,7 +8,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_SERVO, GPIO.OUT)
 
 pwm = GPIO.PWM(GPIO_SERVO, 50) # GPIO 18 als PWM mit 50Hz
-pwm.start(12.5) # Initialisierung (Duty Cylce)
+pwm.start(12) # Initialisierung (Duty Cylce)
 
 print("Start turning...")
 print("Press CTRL-C to stop.")
@@ -16,19 +16,19 @@ print("Press CTRL-C to stop.")
 try:
     while(True):
         print("0 Grad")
-        pwm.ChangeDutyCycle(12.5)
+        pwm.ChangeDutyCycle(12)
         sleep(1)
         print("90 Grad")
         pwm.ChangeDutyCycle(8)
         sleep(1)
         print("180 Grad")
-        pwm.ChangeDutyCycle(3.5)
+        pwm.ChangeDutyCycle(4)
         sleep(1)
         print("90 Grad")
         pwm.ChangeDutyCycle(8)
         sleep(1)
 except KeyboardInterrupt:
-    pwm.ChangeDutyCycle(12.5)
+    pwm.ChangeDutyCycle(12)
     pwm.stop()
     GPIO.cleanup()
 
