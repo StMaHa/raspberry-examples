8848e9896c1335540cad89ecc795662f1c71143c
StMaHa <github@inca-vikingo.de>
2024-04-05 21:13:15 +0200

    Add project of pan-tilt cam on my desk

---
+++ projects/spy-cam/python/show-chair.py
@@ -0,0 +1,33 @@
+# Beispiel mit AngularServo und PiGPIOFactory aus der gpiozero library
+#
+# ATTENTION:
+# Start pigpio daemon first:
+# sudo systemctl start pigpiod
+#
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import AngularServo
+from time import sleep
+
+factory = PiGPIOFactory()
+
+servo_horizontal = AngularServo(12,
+                                min_angle=-90,
+                                max_angle=90,
+                                min_pulse_width=0.0007,
+                                max_pulse_width=0.0023,
+                                pin_factory=factory)
+servo_horizontal.angle= 60
+sleep(1)
+servo_horizontal = None
+
+servo_vertical = AngularServo(18,
+                              min_angle=-90,
+                              max_angle=90,
+                              min_pulse_width=0.0007,
+                              max_pulse_width=0.0023,
+                              pin_factory=factory)
+servo_vertical.angle= 50
+sleep(1)
+servo_vertical = None
+
+print("Done!")
